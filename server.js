a3cfd56231581216dc8d6427f13eb4f727cf4d1a
mosin <jilpa@mosin.(none)>
2019-03-18 15:43:16 +0530

    changes in server file

--- server.js
+++ server.js
@@ -20,15 +20,11 @@ const morgan = require('morgan');
 const path = require('path');
 const rfs = require('rotating-file-stream');
 
-var logDirectory = path.join(__dirname, 'log');
-
-// ensure log directory exists
-fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
 
 // create a rotating write stream
 var accessLogStream = rfs('access.log', {
   interval: '1d', // rotate daily
-  path: logDirectory
+  path: path.join(__dirname, 'log')
 });
 
 // setup the logger
