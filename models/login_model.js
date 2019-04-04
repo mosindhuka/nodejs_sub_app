7399014528897b9ad5c5a159a1b7650af75acfcb
mosin <jilpa@mosin.(none)>
2019-04-04 18:43:34 +0530

    api folder added and deleted some root folders

--- models/login_model.js
+++
@@ -1,5 +0,0 @@
-const db = require('../db');
-module.exports.login = async function (doc) {
-    var results= await db.get().collection('users').find(doc).toArray();
-    return results;
-}
--- routes/index.js
+++
@@ -1,5 +0,0 @@
-// use app in here to bind the routes
-const check_user=require('../middlewares/check_user');
-app.use('/post', check_user.checkUser);
-require('./Login');
-require('./Post');
